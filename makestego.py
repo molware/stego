b9944828ad78843c73a2c717b1b0de2dabaa2b6e
Mo <mo@MJX1C6.localdomain>
2021-07-09 22:43:20 -0400

    Organized to be more repo/user-friendly

--- makestego.py
+++ makestego.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
-from PIL import Image
+from PIL import Image 
+import os
 file=input("Filename: ")
 image=Image.open(file)
 cord=image.load()
+##The text you want to embed in the image
 cleartext="testing"
-#cleartext="testing123456dfshasndvhfhdsafj;hcnsadufvnsuadfpsvanpcufysavnfyvuvdscphfpavnuhasvcfusanhfvcmusavnhcsvdfunvnmcnuvhfcnivaimcashvnicuamfspnvuhacmfuvhsacufhpvcuvhpughvucmdfvughndsmcgvnhsndufhvgdsuihngcfdsuhgmvdsfunghmvcsghr7vnh9n5yv7w5y94tyrunvgh333nvbrewh4234v54n893234n52983475v243n5vb23weyutnuweyntvyqqqqty4b3ntyervyh734nv34v32959yv43n5y43v5y43nfry2394yyyyyyyyyyyyyyyyyyyyyynywr9q823rqewrqpwerpewrywebrwery[[qwr[yweryqwebrqw]qrqrqrquiryqiry"
-#with open('malscript.txt','r') as file:
-#	data=file.read()
-#cleartext=data
-#print(cleartext)
 pointer=0
-
+#Copy the image, and load its pixels
 img2=image
 pixelsNew=img2.load()
 
-for y in range(84):
-        for x in range(150):
+#Modify a specific range of pixels in the image, and then save.
+for y in range(16):
+        for x in range(299):
                 red, green, blue = cord[x,y];
                 char = ord(cleartext[pointer])
                 high = char>>4
@@ -27,4 +25,7 @@ for y in range(84):
                 red, green, blue = cord[x,y]
                 pixelsNew[x,y] = (red,newgreen,newblue)
                 cleartext+=chr(int((blue & 15) * 16 | green & 15))
-img2.save('stego_',"PNG")
+#Save file
+file="stego_" + file                
+stego_path=os.path.join("stego",file)
+img2.save(stego_path,"PNG")
